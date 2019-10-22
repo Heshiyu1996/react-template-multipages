c79b8389c8068da17cc957687ad6fd819187bef3
heshiyu <450979309@qq.com>
2019-10-22 20:04:13 +0800

    feature: Import global.less into index.js

--- src/view/index/index.js
+++ src/view/index/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import NotFound from '@/components/common/not-found';
 import 'core-js'; // 兼容IE处理
 import '@/assets/iconfont/iconfont.css';
+import '@/assets/css/index.less';
 
 const Page = () => (
     <Router>
